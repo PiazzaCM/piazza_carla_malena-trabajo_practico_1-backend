0bbfa71e341ca92c8b210a3a7dd5656835dd00bd
Piazza Carla Malena <carlamalenapiazza@gmail.com>
2023-08-27 00:53:05 -0300

    Se realizo conexion a la base de datos

--- src/app.js
+++ src/app.js
@@ -1,28 +1,30 @@
+//Librerias
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const path = require("path");
+
+
+//config de variables de entorno
 require("dotenv").config();
 
+//conexion a la base de datos
+sequelize.sync();
+sequelize.authenticate().then(console.log('BD conected')).catch(err => console.log(`Se produjo un error: ${err}`));
 
+//inicializar express y asignacion de puerto
 const app = express();
 const port = process.env.PORT || 3000;
 
 
 
-
+//middlewares
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, "public")));
-
-
-
-
-
-
 
 
 
---
+++ src/db/db.js
@@ -0,0 +1,21 @@
+
+const { Sequelize, DataTypes } = require('sequelize');
+
+//configuracion de base de datos
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_DIALECT,
+    }
+  ); 
+
+
+//se exporta la bd
+module.exports = {
+    sequelize,
+    DataTypes
+}
+
